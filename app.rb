42fe116aa4da365d8f5d91e9cb78efc6bf3b298b
Waincey <waincey.chan@gmail.com>
2018-07-17 19:43:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ get '/reversename/:name' do
   @username = [:name]
   @usesrname.reverse
 end
-end 
 
 get '/multiply/:number' do 
   @squarenumber = params[:num1].to_i
   (num1 * num1.to_s)
 end 
-end
+
+
 
