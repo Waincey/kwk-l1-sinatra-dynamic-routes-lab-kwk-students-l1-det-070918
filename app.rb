b9692c01af8941956a5daf0667cff86041049b3a
Waincey <waincey.chan@gmail.com>
2018-07-17 19:39:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,5 +4,9 @@ class App < Sinatra::Base
   # Write your code here!
 get '/reversename/:name' do 
   @username = [:name]
-  #{name}.reverse
+  @usesrname.reverse
 end
+end 
+
+get '/multiply/:num1/:num2' do 
+  num1 = params[:num1].to_i
